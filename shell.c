a0f5c28c682ad5e9e0d318fb80a7c7b7cdde3b0c
Don Vu <don.vu1212@gmail.com>
2017-10-01 17:48:54 -0700

    Implement child process to execute commands

--- shell.c
+++ shell.c
@@ -9,8 +9,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
-void tokenize(char *args[], char *command){
+int tokenize(char *args[], char *command){
     int i = 0;
     
     const char delim[2] = " ";
@@ -23,6 +24,8 @@ void tokenize(char *args[], char *command){
         ++i;
         args[i] = strtok(NULL, delim);
     }
+    
+    return i;
 }
 
 #define MAX_LINE 80
@@ -37,19 +40,44 @@ int main (void){
         fflush(stdout);
         
         char command[MAX_LINE];
-        
+        int num = 0;
         
         fgets(command, (MAX_LINE/2 +1), stdin);
         
-        printf("Your command is: %s", command);
+        printf("Your command is: %s\n", command);
         
         if (!(strcmp(command, exit_str))){
-            should_run = 0; /* User exits the shell */
+            return 0; /* User exits the shell */
         }
         
-        tokenize(args, command);
+        num = tokenize(args, command);
         
+        /* creating child process to run commands */
+        pid_t pid;
         
+        pid = fork();
+        
+        if (pid < 0) { /* error occurred */
+            fprintf(stderr, "Fork failed.");
+            return 1;
+        }
+        else if (pid == 0) { /* child process */
+            execvp(args[0], args);
+            /* error occurred during execvp */
+            fprintf(stderr, "Error running execvp\n");
+            exit(0);
+        }
+        else { /* parent process */
+            /* the parent process will run
+             concurrently if "&" is added to
+             the end of the user's command */
+            if ((strcmp(args[num - 1], "&") != 0)){
+                wait(NULL);
+            }
+            
+        }
+        memset(command, '\0', MAX_LINE);
+        memset(args, '\0', MAX_LINE/2 + 1);
     }
     
     return 0;
